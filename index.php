d58c27f302d7592f966eacab9afbf38e78e3ebbf
Axeman77 <axeman@bk.ru>
2018-03-17 22:09:26 +0300

    HW1

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+$name= "user";
+echo ("Hello, $name! <br>");
+
+define('MY_COST', 100);
+echo MY_COST;
+echo $name;
+
+$int10 = 42;
+$int2 = 0b101010;
+$int8 = 52;
+$int16 = 0x2A;
+
+echo "Десятеричная система $int10 <br>";
+echo "Двоичная система $int2 <br>";
+echo "Восьмеричная система $int8 <br>";
+echo "Шеснатцатиричная система $int16 <br>";
+?>
