35ca9aa567d30b99c61453859ba90d86ff69c500
c205 <axeman@bk.ru>
2018-03-20 13:15:19 +0300

    HW2 start

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
 header('Content-Type: text/html;charset=UTF-8');
 
-
-
 $a = 5;
-$b = '05';
-var_dump($a == $b);         // Почему true?  приводит их к одному типу данных, а затем сравнивает
-var_dump((int)'012345');     // Почему 12345?
-var_dump((float)123.0 === (int)123.0); // Почему false?Думаю, сравнивать не должен
-var_dump((int)0 === (int)'hello, world'); // Почему true? Думаю, что оба значения в чилхах приведены к нулю, хотя сравнивать не должен тождественное равенство
+$b = 7;
 
-$titile = 'Titele 1';
-$content = 'HW';
-?>
 
-<h1><?php echo $titile?></h1>
-<p><?= $content?></p>
+?>;
