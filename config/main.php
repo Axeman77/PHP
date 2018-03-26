67db362af819c808f9ded8383f059abba8fb807c
c205 <axeman@bk.ru>
2018-03-26 15:39:54 +0300

    HW4-создаем структуру проекта;

---
+++ config/main.php
@@ -0,0 +1,5 @@
+<?php
+
+define("ROOT_DIR", __DIR__ . "/..");
+define("PUBLIC_DIR", ROOT_DIR . "/public");
+define("ENGINE_DIR", ROOT_DIR . "/engine");
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type:text/html; charset=utf-8;');
-include_once "../engine/render.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php";
+require_once ENGINE_DIR . "/render.php";
 
 
 displayHello();
