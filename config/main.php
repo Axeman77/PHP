962da84905e567e70f6cf005b790102fb3d9b0b8
c205 <axeman@bk.ru>
2018-03-27 15:48:29 +0300

    HW4

--- config/main.php
+++ config/main.php
@@ -2,3 +2,6 @@
 define("ROOT_DIR", __DIR__ . "/..");
 define("PUBLIC_DIR", ROOT_DIR . "/public");
 define("ENGINE_DIR", ROOT_DIR . "/engine");
+define("UPLOADS_DIR", ROOT_DIR . "/uploads");
+define("TEMPLATES_DIR", ROOT_DIR . "/templates");
+
--- public/index.php
+++ public/index.php
@@ -2,5 +2,21 @@
 header('Content-Type:text/html; charset=utf-8;');
 include_once $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php";
 require_once ENGINE_DIR . "/render.php";
+include_once TEMPLATES_DIR . "/upload_form.php";
 
-displayHello();
+
+
+$dir = opendir("img");
+while ($file = readdir($dir)){
+    if(is_dir($file)){
+        echo "<strong>$file</strong><br>";
+    } else {
+        echo "$file<br>";
+    }
+};
+closedir($dir);
+
+
+
+?>
+
