85416b146b2febc98af59c7739a0123d22997511
c205 <axeman@bk.ru>
2018-04-11 14:38:55 +0300

    HW8

--- pages/cart/add_to_cart.php
+++ pages/cart/add_to_cart.php
@@ -6,4 +6,13 @@ if(isset($_SESSION['cart'][$itemId])){
 }else{
     $_SESSION['cart'][$itemId] = 1;
 }
+
+if (isset($_POST['cart_add'])) {
+    $id = $_POST['id'];
+    session_start();
+    $_SESSION['cart'][$itemId]++;
+}
+
 echo json_encode(["status" => "ok", "message" => "Товар добавлен"]);
+
+
--- templates/cart.php
+++ templates/cart.php
@@ -1,18 +1,52 @@
 <div class=cart>
   <h1>Корзина товаров:</h1>
-    <?php if (empty($cart)): ?>
-      <div>Корзина пуста!</div>
-    <?php else: ?>
-        <?php foreach ($cart as $item): ?>
-        <div>
-          Наименование: <?= $item['name'] ?>;<br>
-          Количество: <?= $item['count'] ?>
-            <? $item['name'] ?><br>
-          <img width="200" src="/images/<?= $item['path'] ?>" alt="img"><br>
-          Описание: <?= $item['txt'] ?> <br>
-          Цена: <?= $item['price'] ?> $<br>
-          <a href="/cart/remove_from_cart?id=<?= $item['id']; ?>">Удалить</a>
-        </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
-
+  <div class="cart-items">
+      <?php if (empty($cart)): ?>
+        <div>Корзина пуста!</div>
+      <?php else: ?>
+          <?php foreach ($cart as $item): ?>
+          <div class="cart-item">
+            Наименование: <?= $item['name'] ?>;<br>
+            Количество: <?= $item['count'] ?>
+              <? $item['name'] ?><br>
+            <img width="200" src="/images/<?= $item['path'] ?>" alt="img"><br>
+            Описание: <?= $item['txt'] ?> <br>
+            Цена: <?= $item['price'] ?> $<br>
+            Количество:
+            <form action="" method="post" class="quantity">
+              <div>
+                <button class="remove" data-id="<?= $item['id'] ?>">-</button>
+              </div>
+              <div>
+                <input type="text" name="quantity" value="1" size="2"/>
+              </div>
+              <div>
+                <button class="add" data-id="<?= $item['id']; var_dump($_POST); ?>">+</button>
+              </div>
+            </form>
+            <a href="/cart/remove_from_cart?id=<?= $item['id']; ?>">Удалить</a>
+          </div>
+          <?php endforeach; ?>
+      <?php endif; ?>
+  </div>
+  <script>
+      $(function () {
+          $(".add").on("click", function () {
+              var id = $(this).data('id');
+              $.ajax({
+                  url: "/cart/add_to_cart",
+                  type: "POST",
+                  data: {
+                      cart_add: true,
+                      id: id
+                  },
+                  success: function (response) {
+                      response = JSON.parse(response);
+                      if (response.status == "ok") {
+                          alert("Товар добавлен в корзину");
+                      }
+                  }
+              });
+          });
+      });
+  </script>
